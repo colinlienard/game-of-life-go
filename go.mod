0ac711aa69e71ba7d620b89431b6b37039ef1446
Colin Lienard <colin.lienard87@gmail.com>
2025-07-18 21:22:23 +0200

    feat: camera

--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module game-of-life
 
 go 1.24.3
 
+require github.com/gen2brain/raylib-go/raylib v0.55.1
+
 require (
 	github.com/ebitengine/purego v0.8.4 // indirect
-	github.com/gen2brain/raylib-go/raylib v0.55.1 // indirect
 	golang.org/x/exp v0.0.0-20250717185816-542afb5b7346 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,36 +1,73 @@
 package main
 
 import (
-	rl "github.com/gen2brain/raylib-go/raylib"
+	"fmt"
+
+	r "github.com/gen2brain/raylib-go/raylib"
 )
 
+const CELL_SIZE float32 = 20
+const CAMERA_ZOOM_SPEED float32 = 0.1
+
 type Cell struct {
-	X int32
-	Y int32
+	X float32
+	Y float32
 }
 
-func main() {
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
-	defer rl.CloseWindow()
+var camera = r.Camera2D{Zoom: 1}
+var panStart = r.Vector2{}
+var cameraPanStart = r.Vector2{}
 
-	rl.SetTargetFPS(120)
+func main() {
+	r.InitWindow(800, 450, "Game of Life")
+	defer r.CloseWindow()
+
+	r.SetTargetFPS(120)
 
 	cells := []Cell{}
 
-	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
+	for !r.WindowShouldClose() {
+		r.BeginDrawing()
 
-		rl.ClearBackground(rl.Black)
+		r.BeginMode2D(camera)
 
-		if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
-			mousePos := rl.GetMousePosition()
-			cells = append(cells, Cell{X: int32(mousePos.X), Y: int32(mousePos.Y)})
+		r.ClearBackground(r.Black)
+
+		mousePos := r.GetMousePosition()
+
+		if r.IsMouseButtonPressed(r.MouseButtonLeft) {
+			fmt.Println(mousePos)
+			x := float32(int((mousePos.X-camera.Offset.X)/camera.Zoom/CELL_SIZE)) * CELL_SIZE
+			y := float32(int((mousePos.Y-camera.Offset.Y)/camera.Zoom/CELL_SIZE)) * CELL_SIZE
+			cells = append(cells, Cell{X: x, Y: y})
+		}
+
+		fmt.Println(camera.Zoom)
+		scroll := r.GetMouseWheelMoveV().Y
+		if scroll < 0 && camera.Zoom-CAMERA_ZOOM_SPEED > 0.1 {
+			camera.Zoom -= CAMERA_ZOOM_SPEED
+			// camera.Offset.X = float32(r.GetScreenWidth()) * CAMERA_ZOOM_SPEED
+			// camera.Offset.Y = float32(r.GetScreenHeight()) * CAMERA_ZOOM_SPEED
+		} else if scroll > 0 {
+			camera.Zoom += CAMERA_ZOOM_SPEED
+		}
+
+		if r.IsMouseButtonPressed(r.MouseButtonMiddle) {
+			panStart.X = mousePos.X
+			panStart.Y = mousePos.Y
+			cameraPanStart.X = camera.Offset.X
+			cameraPanStart.Y = camera.Offset.Y
+		} else if r.IsMouseButtonDown(r.MouseButtonMiddle) {
+			camera.Offset.X = cameraPanStart.X - (panStart.X - mousePos.X)
+			camera.Offset.Y = cameraPanStart.Y - (panStart.Y - mousePos.Y)
 		}
 
 		for _, cell := range cells {
-			rl.DrawRectangle(cell.X, cell.Y, 20, 20, rl.White)
+			r.DrawRectanglePro(r.Rectangle{X: cell.X, Y: cell.Y, Width: CELL_SIZE, Height: CELL_SIZE}, r.Vector2{}, 0, r.White)
 		}
 
-		rl.EndDrawing()
+		r.EndMode2D()
+
+		r.EndDrawing()
 	}
 }
